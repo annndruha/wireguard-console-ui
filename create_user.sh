2235b023ce4685bbdfcc33f69817daf7b392963b
Andrey Marakulin <annndruha.github@gmail.com>
2023-04-18 00:45:15 +0300

    Rofl

--- create_user.sh
+++ create_user.sh
@@ -6,10 +6,26 @@ SERVER_PVKEY=$(cat serverprivatekey)           # Read file with server private k
 SERVER_PBKEY=$(echo $SERVER_PVKEY | wg pubkey) # Calculate server publickey for clients configs
 
 # ===================================================================================================================
+# Yes. It's a python in shell script. Sorry for this.
+get_new_ip="get_new_ip.py"
+echo "import glob" >>$get_new_ip
+echo "files = glob.glob('clients/*.conf')" >>$get_new_ip
+echo "allocated = []" >>$get_new_ip
+echo "for file in files:" >>$get_new_ip
+echo "    with open(file, 'r') as f:" >>$get_new_ip
+echo "        lines = f.readlines()" >>$get_new_ip
+echo "    ip = lines[2].replace('Address = ', '').split('/')[0]" >>$get_new_ip
+echo "    last_number = int(ip.split('.')[3])" >>$get_new_ip
+echo "    allocated.append(last_number)" >>$get_new_ip
+echo "available = list(set(range(2, 256)) - set(allocated))" >>$get_new_ip
+echo "print(available[0])" >>$get_new_ip
+ld=$(python3 $get_new_ip)
+rm $get_new_ip
+
+# ===================================================================================================================
 # Read new username
 echo "\033[0;32mWelcome to andrey cool script!\033[0m "
 mkdir "clients" 2>/dev/null
-ld=$(python3 get_new_ip.py)
 
 read -r -p "New username: " name
 client_conf="clients/${name}.conf"
@@ -18,7 +34,6 @@ if [ -e "$client_conf" ]; then
   echo "\033[0;31mUser with this name already exist. Abort.\033[0m"
   exit 1
 fi
-
 # ===================================================================================================================
 # Generate new client
 
