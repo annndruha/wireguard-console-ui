b22bdd07e2c2d8d1b0e2992618d2913e70fd0b04
Andrey Marakulin <annndruha.github@gmail.com>
2023-04-17 21:29:21 +0300

    Refactor

--- statisitc.sh
+++ statisitc.sh
@@ -1,22 +1,20 @@
 filestat="statistic.txt"
-wg show > ${filestat}
+wg show >"${filestat}"
 
-sed -i "s*peer: **" $filestat
+sed -i "s*peer: **" "${filestat}"
 
-for filename in $(ls clients/)
-do
-n=1
-while read line; do
-	if [ $n = 2 ]; then
-		key=$(echo $line | cut -c 14-)
-		pubkey=$(echo ${key} | wg pubkey)
-
-		shortpeer=$(echo $filename | rev | cut -c 6- | rev) 
-		expression="s*$pubkey*$shortpeer*"
-		sed -i $expression $filestat
-	fi
-	n=$((n+1))
-done < clients/$filename
+for filepath in clients/*.conf; do
+  n=1
+  while read -r line; do
+    if [ $n = 2 ]; then
+      key=$(echo "$line" | cut -c 14-)
+      pubkey=$(echo "${key}" | wg pubkey)
+      short_peer=$(echo "$filepath" | cut -c 9- | rev | cut -c 6- | rev)
+      expression="s*$pubkey*$short_peer*"
+      sed -i "$expression" "${filestat}"
+    fi
+    n=$((n + 1))
+  done <"$filepath"
 done
-cat ${filestat}
-rm ${filestat}
+cat "${filestat}"
+rm "${filestat}"
