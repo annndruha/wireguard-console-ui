27ad572b2653f03484bac694b81176d575d4b6fa
Andrey Marakulin <annndruha.github@gmail.com>
2023-04-17 21:34:59 +0300

    refactor

--- backup_statistic.sh
+++ backup_statistic.sh
@@ -1,18 +1,2 @@
 filestat="stat_$(date +%F__%H-%M-%S).txt"
-wg show >"${filestat}"
-
-sed -i "s*peer: **" "${filestat}"
-
-for filepath in clients/*.conf; do
-  n=1
-  while read -r line; do
-    if [ $n = 2 ]; then
-      key=$(echo "$line" | cut -c 14-)
-      pubkey=$(echo "${key}" | wg pubkey)
-      short_peer=$(echo "$filepath" | cut -c 9- | rev | cut -c 6- | rev)
-      expression="s*$pubkey*$short_peer*"
-      sed -i "$expression" "${filestat}"
-    fi
-    n=$((n + 1))
-  done <"$filepath"
-done
+sh statisitc.sh > "$filestat"
