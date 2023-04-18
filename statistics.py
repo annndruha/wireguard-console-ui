0d574fe427d9b9db880c2673041880106329f2f8
Andrey Marakulin <annndruha.github@gmail.com>
2023-04-18 03:26:39 +0300

    Add stats calc

---
+++ statistics.py
@@ -0,0 +1,53 @@
+import glob
+
+files = glob.glob('statistic/stat_*.txt')
+
+convert = {
+    'B': 1,
+    'KiB': 2 ** 10,
+    'MiB': 2 ** 20,
+    'GiB': 2 ** 30,
+    'TiB': 2 ** 40
+}
+
+
+def parce(transfer_line):
+    data = transfer_line.split('received,')
+    text_r = data[0].replace('transfer:', '').strip()
+    text_s = data[1].replace('sent', '').strip()
+    r = float(text_r.split(' ')[0]) * convert[text_r.split(' ')[1]]
+    s = float(text_s.split(' ')[0]) * convert[text_s.split(' ')[1]]
+    return s, r
+
+
+sent = dict()
+received = dict()
+for file in files:
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    for i, line in enumerate(lines):
+        if line.strip().startswith('transfer'):
+            user = lines[i - 4].strip()
+            s, r = parce(line)
+            if user not in sent:
+                sent[user] = s
+                received[user] = r
+            else:
+                sent[user] += s
+                received[user] += r
+
+
+# sum_traffic = dict()
+# for user in sent.keys():
+#     sum_traffic[user] = (sent[user], received[user], sent[user] + received[user])
+
+
+for name, s, r in zip(sent.keys(), sent.values(), received.values()):
+    tail = ' ' * (27 - len(name))
+    if tail == '':
+        name = name[:24] + '...'
+    s_text = f"{s / (2 ** 30):.2f}"
+    s_text = ' ' * (6 - len(s_text)) + s_text
+    r_text = f"{r / (2 ** 30):.2f} "
+    r_text = ' ' * (8 - len(r_text)) + r_text
+    print(f"{name}{tail}{s_text} GiB{r_text} GiB")
