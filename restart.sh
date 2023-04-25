97e6af5264ccf409aa9f80f5f336dda68e2a6012
Andrey Marakulin <annndruha.github@gmail.com>
2023-04-26 00:03:15 +0300

    Update stats

--- restart.sh
+++ restart.sh
@@ -47,7 +47,21 @@ echo "Total clients: \033[0;32m$total_files\033[0m"
 # Backup statistics
 mkdir "statistic" 2> /dev/null
 filestat="statistic/stat_$(date +%F__%H-%M-%S).txt"
-sh statistic.sh > "$filestat"
+wg show > "${filestat}"
+sed -i "s*peer: **" "${filestat}"
+for filepath in clients/*.conf; do
+  n=1
+  while read -r line; do
+    if [ $n = 2 ]; then
+      key=$(echo "$line" | cut -c 14-)
+      pubkey=$(echo "${key}" | wg pubkey)
+      short_peer=$(echo "$filepath" | cut -c 9- | rev | cut -c 6- | rev)
+      expression="s*$pubkey*$short_peer*"
+      sed -i "$expression" "${filestat}"
+    fi
+    n=$((n + 1))
+  done <"$filepath"
+done
 
 # ===================================================================================================================
 # Restart interface
